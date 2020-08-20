e78d5182e7da5d26cacdb9cca56abc46c6bce9e8
Atsushi Kikuchi <nzlhl1d19@gmail.com>
2020-08-20 16:02:56 +0900

    fix:*.js

--- pages/index.js
+++ pages/index.js
@@ -3,51 +3,51 @@ import Head from 'next/head'
 import Post from '../components/post'
 
 const client = require('contentful').createClient({
-    space: process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID,
-    accessToken: process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN
+  space: process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID,
+  accessToken: process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN
 })
 
 function HomePage() {
-    async function fetchEntries() {
-        const entries = await client.getEntries()
-        if (entries.items) return entries.items
+  async function fetchEntries() {
+    const entries = await client.getEntries()
+    if (entries.items) return entries.items
     console.log(`Error getting Entries for ${contentType.name}.`)
-    }
+  }
 
-    const [posts, setPosts] = useState([])
+  const [posts, setPosts] = useState([])
 
-    useEffect(() => {
-        async function getPosts() {
-        const allPosts = await fetchEntries()
-        setPosts([...allPosts])
+  useEffect(() => {
+    async function getPosts() {
+      const allPosts = await fetchEntries()
+      setPosts([...allPosts])
     }
     getPosts()
-    }, [])
+  }, [])
 
-    return (
-        <>
-        <Head>
-            <title>Next.js + Contentful</title>
-            <link
-                rel="stylesheet"
-                href="https://css.zeit.sh/v1.css"
-                type="text/css"
+  return (
+    <>
+      <Head>
+        <title>Next.js + Contentful</title>
+        <link
+          rel="stylesheet"
+          href="https://css.zeit.sh/v1.css"
+          type="text/css"
+        />
+      </Head>
+      {posts.length > 0
+        ? posts.map(p => (
+            <Post
+              alt={p.fields.alt}
+              date={p.fields.date}
+              key={p.fields.title}
+              image={p.fields.image}
+              title={p.fields.title}
+              url={p.fields.url}
             />
-        </Head>
-        {posts.length > 0
-            ? posts.map(p => (
-                <Post
-                    alt={p.fields.alt}
-                    date={p.fields.date}
-                    key={p.fields.title}
-                    image={p.fields.image}
-                    title={p.fields.title}
-                    url={p.fields.url}
-                />
-            ))
+          ))
         : null}
     </>
-    )
+  )
 }
 
 export default HomePage
